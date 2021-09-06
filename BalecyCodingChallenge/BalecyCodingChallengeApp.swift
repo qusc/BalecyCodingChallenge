b4558bbf488f816b9edafc30c26efbd7da0902cf
Quirin Schweigert <quirin.schweigert@outlook.com>
2021-09-06 10:07:44 +0200

    Initial Commit

---
+++ BalecyCodingChallenge/BalecyCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalecyCodingChallengeApp.swift
+//  BalecyCodingChallenge
+//
+//  Created by Quirin Schweigert on 06.09.21.
+//
+
+import SwiftUI
+
+@main
+struct BalecyCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
